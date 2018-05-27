afc96ed809067fb5bfa4f6dab3a623c97883055a
lpatmo <linda.peng@alumni.duke.edu>
2018-05-26 19:25:23 -0700

    semantic ui setup

--- bookworm-react/src/App.js
+++ bookworm-react/src/App.js
@@ -4,9 +4,11 @@ import HomePage from "./components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
 
 
-const App = () => <div>
-    <Route path="/" exact component={HomePage} />
-    <Route path="/login" exact component={LoginPage} />
-</div>;
+const App = () => (
+    <div className="ui container">
+        <Route path="/" exact component={HomePage} />
+        <Route path="/login" exact component={LoginPage} />
+    </div>
+);
 
 export default App;
