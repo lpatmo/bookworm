6819e1e6eb8d7e93ad24a0a04dd1de3a1908988f
lpatmo <linda.peng@alumni.duke.edu>
2018-05-26 20:11:43 -0700

    validate form and submit datag

--- bookworm-react/src/components/pages/LoginPage.js
+++ bookworm-react/src/components/pages/LoginPage.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import LoginForm from "../forms/LoginForm";
 
-const LoginPage = () => (
-	<div>
-		<h1>Login Page</h1>
-	</div>
-);
+class LoginPage extends React.Component {
+    submit = data => {
+        console.log(data);
+    }
+    
+    render() {
+        return(
+    	    <div>
+        		<h1>Login Page</h1>
+                <LoginForm submit = { this.submit }/>
+        	</div>
+        );
+    }
+}
+
+
 
 export default LoginPage;
